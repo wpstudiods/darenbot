f877d75f62446086e175cac6c4475dfad2a12d95
wpstudiods <danilastepovois@gmail.com>
2018-04-16 20:37:16 +0300

    Удаляем мусор: rss.js

--- rss.js
+++
@@ -1,6 +0,0 @@
-let RssFeedEmitter = require('rss-feed-emitter');
-let feeder = new RssFeedEmitter();
-feeder.add({
-  url: 'https://www.youtube.com/feeds/videos.xml?channel_id=UCcJFUDfukVWvTfkILUu2nzg',
-  refresh: 2000
-});
